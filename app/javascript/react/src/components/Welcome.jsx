1462520bea9ebe95a99118fb578f53f3d01814db
shashankjo <kumarshashank.jo@gmail.com>
2023-07-23 18:37:53 +0530

    Working with React Components - Iterating Lists

--- app/javascript/react/src/components/Welcome.jsx
+++ app/javascript/react/src/components/Welcome.jsx
@@ -1,10 +1,12 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
+import QuestionList from './QuestionList'
 
 const Welcome = () => {
   return (
     <div className="container">
       <h1>Welcome to Rails 7 with React JS course</h1>
+      <QuestionList/>
     </div>
   )
 }
