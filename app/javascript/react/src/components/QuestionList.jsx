a652afcda9d6bfe2ca2cb5d9d7005d960a3d9a0d
shashankjo <kumarshashank.jo@gmail.com>
2023-07-29 15:44:09 +0530

    Understanding Keys in React

--- app/javascript/react/src/components/QuestionList.jsx
+++ app/javascript/react/src/components/QuestionList.jsx
@@ -4,31 +4,36 @@ import * as ReactDOM from 'react-dom'
 const QuestionList = () => {
     const questionList = [
         {
-            title: 'How to check if a key is present in a Hash?',
-            tag: 'Ruby'
+          id: 1,
+          title: 'How to check if a key is present in a Hash?',
+          tag: 'Ruby'
         },
         {
-            title: 'What is the difference between string and a symbol?',
-            tag: 'Ruby'
+          id: 2,
+          title: 'What is the difference between string and a symbol?',
+          tag: 'Ruby'
         },
         {
-            title: 'What will happen if we add two keys with the same name in a Hash?',
-            tag: 'Ruby'
+          id: 3,
+          title: 'What will happen if we add two keys with the same name in a Hash?',
+          tag: 'Ruby'
         },
         {
-            title: 'How to delete a given key from a Hash?',
-            tag: 'Ruby'
+          id: 4,
+          title: 'How to delete a given key from a Hash?',
+          tag: 'Ruby'
         },
         {
-            title: 'How to check if two hashes are identical?',
-            tag: 'Ruby'
+          id: 5,
+          title: 'How to check if two hashes are identical?',
+          tag: 'Ruby'
         }
     ]
   return (
     <div className='row'>
       <div className='col-lg-10 mx-auto'>
         {questionList.map((question) =>
-        <div className='card rounded-0 mt-3'>
+        <div className='card rounded-0 mt-3' key={question.id}>
           <div className='card-body'>
             <h3 className='card-title'>{question.title}</h3>
             <p className='lead'><span className='badge bg-primary'>{question.tag}</span>
