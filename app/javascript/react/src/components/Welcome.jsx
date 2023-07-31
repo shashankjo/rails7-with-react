3cd1747158c40475a70fd4554f727e36a61b4114
shashankjo <kumarshashank.jo@gmail.com>
2023-07-31 12:30:43 +0530

    Replacing ReactDOM.render with createRoot

--- app/javascript/react/src/components/Welcome.jsx
+++ app/javascript/react/src/components/Welcome.jsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import * as ReactDOM from 'react-dom'
+import * as ReactDOM from 'react-dom/client'
 import QuestionList from './QuestionList'
 
 const Welcome = () => {
@@ -11,8 +11,11 @@ const Welcome = () => {
   )
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-  ReactDOM.render(<Welcome/>, document.getElementById('welcome'))
-})
+const root = ReactDOM.createRoot(document.getElementById('welcome'))
+root.render(
+  <React.StrictMode>
+    <Welcome/>
+  </React.StrictMode>
+)
 
 export default Welcome;
