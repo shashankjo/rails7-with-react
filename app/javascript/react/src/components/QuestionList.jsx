95317b71755dd1e5ce5d1f3a140afa8327e30b5a
Kumar Shashank <40141542+shashankjo@users.noreply.github.com>
2023-07-31 11:30:48 +0530

    Using and understanding Props in React (#7)

--- app/javascript/react/src/components/QuestionList.jsx
+++ app/javascript/react/src/components/QuestionList.jsx
@@ -1,5 +1,6 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
+import QuestionDetail from './QuestionDetail'
 
 const QuestionList = () => {
     const questionList = [
@@ -33,13 +34,7 @@ const QuestionList = () => {
     <div className='row'>
       <div className='col-lg-10 mx-auto'>
         {questionList.map((question) =>
-        <div className='card rounded-0 mt-3' key={question.id}>
-          <div className='card-body'>
-            <h3 className='card-title'>{question.title}</h3>
-            <p className='lead'><span className='badge bg-primary'>{question.tag}</span>
-            </p>
-          </div>
-        </div>
+        <QuestionDetail question={question} key={question.id}/>
         )}
       </div>
     </div>
