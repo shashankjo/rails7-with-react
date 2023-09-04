d049ae2100b4abb08052e98f4ef136a4f9cab414
shashankjo <kumarshashank.jo@gmail.com>
2023-09-05 00:36:22 +0530

    Implement Like-Dislike Button for Questions

--- app/javascript/react/src/components/QuestionDetail.jsx
+++ app/javascript/react/src/components/QuestionDetail.jsx
@@ -5,14 +5,28 @@ class QuestionDetail extends React.Component {
 
   constructor(props){
     super(props)
-    this.state = { likeCount: 0 }
+    this.state = {
+      likeCount: 0,
+      dislikeCount: 0
+    }
     this.updateLikeCount = this.updateLikeCount.bind(this)
+    this.updateDisLikeCount = this.updateDisLikeCount.bind(this)
   }
 
-  updateLikeCount(){
-    this.setState(function(state) {
+  updateLikeCount = () => {
+    this.setState((state) => {
+      console.log("shashankkkkkkkk")
       return{
         likeCount: state.likeCount + 1
+      }
+    })
+  }
+
+  updateDisLikeCount = () => {
+    this.setState((state) => {
+      console.log("shashank")
+      return{
+        dislikeCount: state.dislikeCount + 1
       }
     })
   }
@@ -24,11 +38,18 @@ class QuestionDetail extends React.Component {
           <h3 className='card-title'>{this.props.question.title}</h3>
           <p className='lead'><span className='badge bg-primary'>{this.props.question.tag}</span>
           </p>
-          <button className='btn btn-primary mt-2' onClick= {this.updateLikeCount}>Like</button>
-          {
-            this.state.likeCount > 0 ?
-            <span className='badge bg-info'>{this.state.likeCount}</span> : ""
-          }
+          <button type="button" className="btn btn-primary position-relative" onClick={this.updateLikeCount} style={{marginRight: 1 + 'em'}}>
+            Like
+            { this.state.likeCount > 0 ?
+              <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-success">{this.state.likeCount}</span> : ''
+            }
+          </button>
+          <button type="button" className="btn btn-danger position-relative" onClick={this.updateDislikeCount}>
+            Dislike
+            { this.state.dislikeCount > 0 ?
+              <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">{this.state.dislikeCount}</span> : ''
+            }
+          </button>
         </div>
       </div>
     )
