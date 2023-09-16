e40c89df74cfef0624da1f6a11cefa6fcb277c11
shashankjo <kumarshashank.jo@gmail.com>
2023-09-16 20:31:20 +0530

    Add dropdown to select question tags to display the filtered results

--- app/javascript/react/src/components/QuestionList.jsx
+++ app/javascript/react/src/components/QuestionList.jsx
@@ -4,7 +4,19 @@ import * as ReactDOM from 'react-dom'
 import QuestionDetail from './QuestionDetail'
 
 const QuestionList = () => {
+
+  const questionsTags = [
+    { label: 'All', value: 0 },
+    { label: 'Ruby', value: 1 },
+    { label: 'Rails', value: 2 },
+    { label: 'React', value: 3 },
+    { label: 'Bootstrap', value: 4 },
+    { label: 'Javascript', value: 5 }
+  ]
+
   const [questionList, setQuestionList] = useState([])
+  const [selectedOption, setSelectedOption] = useState(questionsTags[0].value)
+
   const questionsUrl = 'http://localhost:3000/api/v1/questions'
 
   const fetchQuestionsList = () => {
@@ -20,9 +32,29 @@ const QuestionList = () => {
     fetchQuestionsList()
   }, [])
 
+  const updateSelectedItem = (event) => {
+    setQuestionList([])
+    setSelectedOption(event.target.value)
+    fetch(questionsUrl + `?tags=${questionsTags[event.target.value].label}`)
+      .then((response) => response.json())
+      .then((data) => {
+        setQuestionList(data)
+      })
+  }
+
   return (
     <div className='row'>
       <div className='col-lg-10 mx-auto'>
+      <p className="lead fw-bold">Filter Questions by Tags</p>
+        <select
+          className="form-select form-select-lg rounded-0"
+          value={selectedOption}
+          onChange={event => updateSelectedItem(event)}>
+          {questionsTags.map(tag => (
+            <option key={tag.value} value={tag.value}>{tag.label}</option>
+          ))}
+        </select>
+
         {questionList.map((question) =>
         <QuestionDetail question={question} key={question.id}/>
         )}
